e1ac1aa7a40697ed6b77930de111e72e31e63c4d
addiswebb <akwgma@gmail.com>
2026-04-28 13:22:10 +0100

    Add global_const_expr_test.c

---
+++ tests/complex/global_const_expr_test.c
@@ -0,0 +1,14 @@
+// RUN: %cc %s -o %t
+// RUN: %t | %check_output "hello world 15"
+
+int printf(const char *, ...);
+
+char *strs[2] = {[0] = "hello", [1] = "world"};
+int a = 10;
+int c = {15};
+int *b = &a + 1;
+
+int main() {
+    printf("%s %s %d\n", strs[0], strs[1], *b);
+    return 0;
+}
