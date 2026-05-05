ba6b50cb7d3ac1a7ade17de193768d8ec4d24d59
addiswebb <akwgma@gmail.com>
2026-05-05 20:10:43 +0100

    Implement string literal concatenation

--- src/core/array.c
+++ src/core/array.c
@@ -15,8 +15,8 @@ void array_init(Array *arr, int initial_capacity, int element_size) {
     arr->count = 0;
 }
 
-static void ensure_capacity(Array *arr) {
-    if (arr->count >= arr->capacity) {
+static void ensure_capacity(Array *arr, int size) {
+    if (size >= arr->capacity) {
         arr->capacity *= 2;
         void *new_data = realloc(arr->data, arr->element_size * arr->capacity);
         if (!new_data) {
@@ -25,24 +25,36 @@ static void ensure_capacity(Array *arr) {
         arr->data = new_data;
     }
 }
+
+/* Conservatively realloc */
+static void reserve_capacity(Array *arr, int size) {
+    ASSERT(size > 0, "Cannot reserve 0 capacity\n");
+    if (size > arr->capacity) {
+        arr->capacity = size;
+        void *new_data = realloc(arr->data, arr->capacity);
+        ASSERT(new_data, "Failed to resize array\n");
+        arr->data = new_data;
+    }
+}
 static void ensure_index(const Array *arr, int index) {
-    #ifdef __COMPILER_C__
+#ifdef __COMPILER_C__
     if (index >= arena->count || index < 0) {
-    #else
+#else
     if (__builtin_expect(index >= arr->count || index < 0, 0)) {
-    #endif
+#endif
         PANIC("Index of %d is out of Array bounds of %d\n", index, arr->count);
     }
 }
 
 void *append(Array *arr, const void *element) {
-    ensure_capacity(arr);
+    ensure_capacity(arr, arr->count);
     memcpy((char *)arr->data + arr->count * arr->element_size, element, arr->element_size);
     return (char *)arr->data + arr->count++ * arr->element_size;
 }
 
 void *insert(Array *arr, const void *element, int index) {
-    ensure_capacity(arr);
+    // TODO max(arr->count, index);
+    ensure_capacity(arr, arr->count);
     ASSERT(index <= arr->capacity && index >= 0, "Index must be within capacity to insert in array\n");
     // ensure_index(arr, index);
 
@@ -85,4 +97,12 @@ void array_str_cpy(Array *arr, const char *str) {
         append(arr, str++);
     }
     append(arr, &(char){'\0'});
+}
+
+void array_str_catn(Array *arr, const char *str, int n) {
+    reserve_capacity(arr, arr->count + n + 1);
+    memcpy(arr->data + arr->count, str, n);
+    arr->count += n;
+    append(arr, &(char){'\0'});
+    arr->count--;
 }
--- tests/test.s
+++ tests/test.s
@@ -1,230 +1,23 @@
 .section .note.GNU-stack,"",@progbits
 .section .rodata
 .LC0:
-    .byte '%', 'd', ' ', 0
-.LC1:
-    .byte 0x0A, 0
+    .byte 'H', 'e', 'l', 'o', 'W', 'o', 'r', 'l', 'd', 0x0A, 'W', 'O', 'W', 'O', ' ', 0x0A, ' ', 'W', 'o', 0x0A, 0
 
 .text
-.global cmp_int
-cmp_int:
-    push %rbp
-    mov %rsp, %rbp
-    subq $48, %rsp
-cmp_int_0:
-    movq %rdi, -8(%rbp)
-    movq %rsi, -16(%rbp)
-    leaq -24(%rbp), %rax
-    movq %rax, -40(%rbp)
-    leaq -8(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movq (%rax), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -48(%rbp)
-    movq -40(%rbp), %rax
-    movl -48(%rbp), %ecx
-    movl %ecx, (%rax)
-    leaq -32(%rbp), %rax
-    movq %rax, -40(%rbp)
-    leaq -16(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movq (%rax), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -48(%rbp)
-    movq -40(%rbp), %rax
-    movl -48(%rbp), %ecx
-    movl %ecx, (%rax)
-    leaq -24(%rbp), %rax
-    movq %rax, -40(%rbp)
-    movq -40(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -40(%rbp)
-    leaq -32(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -48(%rbp)
-    movl -40(%rbp), %eax
-    cmpl -48(%rbp), %eax
-    setl %al
-    movzbl %al, %eax
-    movl %eax, -40(%rbp)
-    movl -40(%rbp), %eax
-    testl %eax, %eax
-    jz cmp_int_2
-cmp_int_1:
-    movl $-1, %eax
-    mov %rbp, %rsp
-    pop %rbp
-    ret
-    jmp cmp_int_2
-cmp_int_2:
-    leaq -24(%rbp), %rax
-    movq %rax, -40(%rbp)
-    movq -40(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -40(%rbp)
-    leaq -32(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -48(%rbp)
-    movl -40(%rbp), %eax
-    cmpl -48(%rbp), %eax
-    setg %al
-    movzbl %al, %eax
-    movl %eax, -40(%rbp)
-    movl -40(%rbp), %eax
-    testl %eax, %eax
-    jz cmp_int_4
-cmp_int_3:
-    movl $1, %eax
-    mov %rbp, %rsp
-    pop %rbp
-    ret
-    jmp cmp_int_4
-cmp_int_4:
-    movl $0, %eax
-    mov %rbp, %rsp
-    pop %rbp
-    ret
 .global main
 main:
     push %rbp
     mov %rsp, %rbp
-    subq $64, %rsp
+    subq $16, %rsp
 main_0:
-    leaq -24(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl $5, %ecx
-    movl %ecx, (%rax)
-    movq -48(%rbp), %rax
-    addq $4, %rax
-    movq %rax, -56(%rbp)
-    movq -56(%rbp), %rax
-    movl $1, %ecx
-    movl %ecx, (%rax)
-    movq -48(%rbp), %rax
-    addq $8, %rax
-    movq %rax, -56(%rbp)
-    movq -56(%rbp), %rax
-    movl $4, %ecx
-    movl %ecx, (%rax)
-    movq -48(%rbp), %rax
-    addq $12, %rax
-    movq %rax, -56(%rbp)
-    movq -56(%rbp), %rax
-    movl $2, %ecx
-    movl %ecx, (%rax)
-    movq -48(%rbp), %rax
-    addq $16, %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl $3, %ecx
-    movl %ecx, (%rax)
-    leaq -32(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl $5, %ecx
-    movl %ecx, (%rax)
-    leaq -24(%rbp), %rax
-    movq %rax, -48(%rbp)
-    leaq -32(%rbp), %rax
-    movq %rax, -56(%rbp)
-    movq -56(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -56(%rbp)
-    movslq -56(%rbp), %rax
-    movq %rax, -56(%rbp)
-    leaq cmp_int(%rip), %rax
-    movq %rax, -64(%rbp)
-    movq -48(%rbp), %rdi
-    movq -56(%rbp), %rsi
-    movq $4, %rdx
-    movq -64(%rbp), %rcx
-    call qsort
-    leaq -40(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl $0, %ecx
-    movl %ecx, (%rax)
-main_1:
-    leaq -40(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -48(%rbp)
-    leaq -32(%rbp), %rax
-    movq %rax, -56(%rbp)
-    movq -56(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -56(%rbp)
-    movl -48(%rbp), %eax
-    cmpl -56(%rbp), %eax
-    setl %al
-    movzbl %al, %eax
-    movl %eax, -48(%rbp)
-    movl -48(%rbp), %eax
-    testl %eax, %eax
-    jz main_4
-main_2:
     leaq .LC0(%rip), %rax
-    movq %rax, -48(%rbp)
-    leaq -24(%rbp), %rax
-    movq %rax, -56(%rbp)
-    leaq -40(%rbp), %rax
-    movq %rax, -64(%rbp)
-    movq -64(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -64(%rbp)
-    movslq -64(%rbp), %rax
-    movq %rax, -64(%rbp)
-    movq -64(%rbp), %rax
-    imulq $4, %rax
-    movq %rax, -64(%rbp)
-    movq -56(%rbp), %rax
-    addq -64(%rbp), %rax
-    movq %rax, -56(%rbp)
-    movq -56(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -56(%rbp)
+    movq %rax, -8(%rbp)
     subq $176, %rsp
-    movq -48(%rbp), %rdi
-    movl -56(%rbp), %esi
+    movq -8(%rbp), %rdi
     xor %eax, %eax
     call printf
     addq $176, %rsp
-    movl %eax, -48(%rbp)
-    jmp main_3
-main_3:
-    leaq -40(%rbp), %rax
-    movq %rax, -48(%rbp)
-    movq -48(%rbp), %rax
-    movl (%rax), %eax
-    movl %eax, -56(%rbp)
-    movl -56(%rbp), %eax
-    addl $1, %eax
-    movl %eax, -56(%rbp)
-    movq -48(%rbp), %rax
-    movl -56(%rbp), %ecx
-    movl %ecx, (%rax)
-    jmp main_1
-main_4:
-    leaq .LC1(%rip), %rax
-    movq %rax, -48(%rbp)
-    subq $176, %rsp
-    movq -48(%rbp), %rdi
-    xor %eax, %eax
-    call printf
-    addq $176, %rsp
-    movl %eax, -48(%rbp)
+    movl %eax, -8(%rbp)
     movl $0, %eax
     mov %rbp, %rsp
     pop %rbp
